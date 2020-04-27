5eca5dcb34eff1fe24a5f54cb911912cc2a8e9fb
Bohne9 <jonah@die-schuellers.de>
2020-04-27 09:35:01 +0200

    Initial Commit

---
+++ Tymed/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tymed
+//
+//  Created by Jonah Schueller on 27.04.20.
+//  Copyright © 2020 Jonah Schueller. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
