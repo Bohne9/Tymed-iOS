a73b727311996ff2d28d981136272410ff534486
Bohne9 <jonah@die-schuellers.de>
2020-06-21 21:33:28 +0200

    Getting some structure into the file organisation of View/Home

---
+++ Tymed/View/CellConfiguartor/BaseCollectionViewCellConfigurator.swift
@@ -0,0 +1,21 @@
+//
+//  BaseCollectionViewCellConfigurator.swift
+//  Tymed
+//
+//  Created by Jonah Schueller on 21.06.20.
+//  Copyright © 2020 Jonah Schueller. All rights reserved.
+//
+
+import Foundation
+
+class BaseCollectionViewCellConfigurator {
+    
+    func reset() {
+        
+    }
+    
+    func reload() {
+        
+    }
+    
+}
