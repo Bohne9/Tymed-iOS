f5dc3552aee8621f4d0c7d146f20450d361574aa
Bohne9 <jonah@die-schuellers.de>
2020-07-05 12:53:30 +0200

    Delivered task due date notifications will be removed if the task is completed.

---
+++ Tymed/View/TableViewCells/TaskDetailCells/TaskNotificationTitleTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TaskNotificationTitleTableViewCell.swift
+//  Tymed
+//
+//  Created by Jonah Schueller on 05.07.20.
+//  Copyright © 2020 Jonah Schueller. All rights reserved.
+//
+
+import UIKit
+
+class TaskNotificationTitleTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
