f258539fe991aed89367df24bc77a3e3cf7e6b54
Bohne9 <jonah@die-schuellers.de>
2020-07-28 17:42:11 +0200

    Timetable Overview scene now has a shortcut button to add further timetables.

--- Tymed/Controller/TimetableEdit/TimetableOverviewCollectionViewController.swift
+++ Tymed/Controller/TimetableEdit/TimetableOverviewCollectionViewController.swift
@@ -47,6 +47,7 @@ class TimetableOverviewCollectionViewController: UITableViewController {
         title = "Timetables"
         
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: addReuseIdentifier)
+        tableView.register(TimetableOverviewAddTableViewViewCell.self, forCellReuseIdentifier: "addTimetable")
         tableView.register(NoTimetablesTableViewCell.self, forCellReuseIdentifier: "noTimetables")
         
         fetchData()
@@ -114,11 +115,11 @@ class TimetableOverviewCollectionViewController: UITableViewController {
         guard let timetables = self.timetables else {
             return 1
         }
-        return max(timetables.count, 1)
+        return max(timetables.count + 1, 1)
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard timetables?.count ?? 0 > 0, let timetable = timetables?[indexPath.row] else {
+        guard timetables?.count ?? 0 > 0 else {
             let cell = tableView.dequeueReusableCell(withIdentifier: "noTimetables", for: indexPath) as! NoTimetablesTableViewCell
             
             cell.addButton.addTarget(self, action: #selector(showTimetableAdd), for: .touchUpInside)
@@ -126,9 +127,20 @@ class TimetableOverviewCollectionViewController: UITableViewController {
             return cell
         }
         
+        guard indexPath.row < self.tableView(tableView, numberOfRowsInSection: indexPath.section) - 1 else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "addTimetable", for: indexPath) as! TimetableOverviewAddTableViewViewCell
+            
+            cell.addButton.addTarget(self, action: #selector(showTimetableAdd), for: .touchUpInside)
+            
+            return cell
+        }
+        
+        let timetable = timetables?[indexPath.row]
+        
         let cell = tableView.dequeueReusableCell(withIdentifier: addReuseIdentifier, for: indexPath)
         
-        cell.textLabel?.text = timetable.name
+        cell.textLabel?.font = UIFont.systemFont(ofSize: 20, weight: .semibold)
+        cell.textLabel?.text = timetable?.name
         cell.accessoryType = .disclosureIndicator
         
         return cell
@@ -159,6 +171,12 @@ class TimetableOverviewCollectionViewController: UITableViewController {
             return
         }
         
+        guard indexPath.row < self.tableView(tableView, numberOfRowsInSection: indexPath.section) - 1 else {
+            tableView.cellForRow(at: indexPath)?.isSelected = false
+            showTimetableAdd()
+            return
+        }
+        
         guard let timetable = self.timetable(for: indexPath) else {
             return
         }
@@ -172,6 +190,41 @@ class TimetableOverviewCollectionViewController: UITableViewController {
     }
 }
 
+class TimetableOverviewAddTableViewViewCell: UITableViewCell {
+    
+    var addButton = UIButton()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        setup()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setup() {
+        
+        contentView.addSubview(addButton)
+        addButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        addButton.constraintCenterXToSuperview(constant: 0)
+        addButton.constraintCenterYToSuperview(constant: 0)
+        
+        addButton.setTitle("Add timetable", for: .normal)
+        addButton.setImage(
+            UIImage(systemName: "plus")?.withConfiguration(UIImage.SymbolConfiguration(pointSize: 18, weight: .semibold))
+            , for: .normal)
+        
+        addButton.setTitleColor(.systemBlue, for: .normal)
+        
+        addButton.titleLabel?.font = .systemFont(ofSize: 18, weight: .semibold)
+        
+//        addButton.isEnabled = false
+        
+    }
+}
 
 extension TimetableOverviewCollectionViewController : TimetableOverviewDeleteDelegate {
     
