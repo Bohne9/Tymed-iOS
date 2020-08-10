11702e2d55055abd6d8f2864de0cae621a5be37d
Bohne9 <jonah@die-schuellers.de>
2020-08-10 20:08:43 +0200

    TimetableDetail view will now show a shortcut to add the first lesson if there isn't a lesson in a timetable.

--- Tymed/View/TimetableDetail.swift
+++ Tymed/View/TimetableDetail.swift
@@ -21,6 +21,9 @@ struct TimetableDetail: View {
     var maxNumberOfSubjects = 4
     var maxNumberOfTasks = 4
     
+    @State private var showLessonAdd = false
+    @State private var showTaskAdd = false
+    
     var body: some View {
         
         List {
@@ -31,63 +34,81 @@ struct TimetableDetail: View {
             
             //MARK: Subjects
             Section(header: Text("Subjects").font(.system(size: 12, weight: .semibold))) {
-                ForEach(subjects().prefix(maxNumberOfSubjects), id: \.self) { subject in
-                    NavigationLink(
-                        destination: Text("Subject detail"),
-                        label: {
-                            HStack {
-                                Circle()
-                                    .foregroundColor(Color(UIColor(subject) ?? .clear))
-                                    .frame(width: 10, height: 10)
-                                Text(subject.name ?? "")
-                                    .font(.system(size: 15, weight: .semibold))
-                                Spacer()
-                                Text("\(subject.lessons?.count ?? 0) lessons")
-                                    .font(.system(size: 12, weight: .semibold))
-                            }.frame(height: 45)
-                        })
+                if subjects().count > 0 {
+                    ForEach(subjects().prefix(maxNumberOfSubjects), id: \.self) { subject in
+                        NavigationLink(
+                            destination: Text("Subject detail"),
+                            label: {
+                                HStack {
+                                    Circle()
+                                        .foregroundColor(Color(UIColor(subject) ?? .clear))
+                                        .frame(width: 10, height: 10)
+                                    Text(subject.name ?? "")
+                                        .font(.system(size: 15, weight: .semibold))
+                                    Spacer()
+                                    Text("\(subject.lessons?.count ?? 0) lessons")
+                                        .font(.system(size: 12, weight: .semibold))
+                                }.frame(height: 45)
+                            })
+                    }
+                    if subjects().count > maxNumberOfSubjects {
+                        NavigationLink(
+                            destination: Text("See all subjects"),
+                            label: {
+                                HStack {
+                                    Text("See all")
+                                    Spacer()
+                                }.foregroundColor(Color(.systemBlue))
+                                .font(.system(size: 15, weight: .semibold))
+                            })
+                    }
+                }else {
+                    HStack {
+                        DetailCellDescriptor("Add your first lesson", image: "plus", .systemBlue)
+                        Spacer()
+                        Image(systemName: "chevron.right")
+                            .font(.system(size: 12, weight: .semibold))
+                            .foregroundColor(Color(.tertiaryLabel))
+                    }
+                    .onTapGesture {
+                        showLessonAdd.toggle()
+                    }
+                    .sheet(isPresented: $showLessonAdd) {
+                        LessonAddView(timetable) {
+                            
+                        }
+                    }
                 }
-                if subjects().count > maxNumberOfSubjects {
-                    NavigationLink(
-                        destination: Text("See all subjects"),
-                        label: {
-                            HStack {
-                                Text("See all")
-                                Spacer()
-                            }.foregroundColor(Color(.systemBlue))
-                            .font(.system(size: 15, weight: .semibold))
-                        })
-                }
-                
             }
             
             //MARK: Tasks
-            Section(header: Text("Tasks").font(.system(size: 12, weight: .semibold))) {
-                ForEach(unarchivedTasks().prefix(maxNumberOfTasks), id: \.self) { task in
-                    NavigationLink(
-                        destination: TaskEditContent(task: task, dismiss: {
-                            
-                        }).navigationBarItems(trailing: EmptyView()) ,
-                        label: {
-                            HStack {
-                                Text(task.title ?? "")
-                                    .font(.system(size: 15, weight: .semibold))
-                                Spacer()
-                            }.frame(height: 45)
-                        })
+            if unarchivedTasks().count > 0 {
+                Section(header: Text("Tasks").font(.system(size: 12, weight: .semibold))) {
+                    ForEach(unarchivedTasks().prefix(maxNumberOfTasks), id: \.self) { task in
+                        NavigationLink(
+                            destination: TaskEditContent(task: task, dismiss: {
+                                
+                            }).navigationBarItems(trailing: EmptyView()) ,
+                            label: {
+                                HStack {
+                                    Text(task.title)
+                                        .font(.system(size: 15, weight: .semibold))
+                                    Spacer()
+                                }.frame(height: 45)
+                            })
+                    }
+                    if unarchivedTasks().count > maxNumberOfTasks {
+                        NavigationLink(
+                            destination: Text("See all tasks"),
+                            label: {
+                                HStack {
+                                    Text("See all")
+                                    Spacer()
+                                }.foregroundColor(Color(.systemBlue))
+                                .font(.system(size: 15, weight: .semibold))
+                            })
+                    }
                 }
-                if unarchivedTasks().count > maxNumberOfTasks {
-                    NavigationLink(
-                        destination: Text("See all tasks"),
-                        label: {
-                            HStack {
-                                Text("See all")
-                                Spacer()
-                            }.foregroundColor(Color(.systemBlue))
-                            .font(.system(size: 15, weight: .semibold))
-                        })
-                }
-                
             }
             
             //MARK: Archived tasks
