d43d81dd620fbec1c4cfb4a2ac0bac1ee3c3ae07
Bohne9 <jonah@die-schuellers.de>
2020-09-05 22:30:39 +0200

    Fixes a bug where the TimetableAddView would not be able to save the timetable in core data since not all required fields have a value provided.

--- Tymed/View/Timetable/TimetableAddView.swift
+++ Tymed/View/Timetable/TimetableAddView.swift
@@ -39,6 +39,8 @@ struct TimetableAddView: View {
                     }
                 }
                 
+                Text("Quick setup")
+                
                 //MARK: Subjects
                 ForEach(subjects) { subject in
                     
