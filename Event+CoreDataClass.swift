eada7e9c861d8b7889871d04dd400f51c14c5603
Jonah Schüller <jonah@die-schuellers.de>
2020-09-08 21:12:19 +0200

    Adds an event model to the core data model.

---
+++ Event+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Event+CoreDataClass.swift
+//  Tymed
+//
+//  Created by Jonah Schueller on 08.09.20.
+//  Copyright © 2020 Jonah Schueller. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Event)
+public class Event: NSManagedObject {
+
+}
---
+++ Event+CoreDataProperties.swift
@@ -0,0 +1,37 @@
+//
+//  Event+CoreDataProperties.swift
+//  Tymed
+//
+//  Created by Jonah Schueller on 08.09.20.
+//  Copyright © 2020 Jonah Schueller. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Event {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Event> {
+        return NSFetchRequest<Event>(entityName: "Event")
+    }
+
+    @NSManaged public var title: String?
+    @NSManaged public var start: Date?
+    @NSManaged public var end: Date?
+    @NSManaged public var createdAt: Date?
+    @NSManaged public var body: String?
+    @NSManaged public var notes: String?
+    @NSManaged public var notification: Date?
+    @NSManaged public var id: UUID?
+    @NSManaged public var task: Task?
+    @NSManaged public var subject: Subject?
+    @NSManaged public var lesson: Lesson?
+    @NSManaged public var timetable: Timetable?
+
+}
+
+extension Event : Identifiable {
+
+}
