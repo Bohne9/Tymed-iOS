e79b5fb206c51b249602a699d8be446e9bbc1615
Jonah Schüller <jonah@die-schuellers.de>
2020-09-29 13:53:07 +0200

    A first implementation of the CalendarView.

--- Tymed/View/Calendar/CalendarView.swift
+++ Tymed/View/Calendar/CalendarView.swift
@@ -9,13 +9,57 @@
 import SwiftUI
 
 struct CalendarView: View {
+    
+    @StateObject
+    var calendarViewModel = CalendarViewModel()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+        
+        NavigationView {
+            TabView(selection: $calendarViewModel.index) {
+                ForEach(calendarViewModel.dayEntries.indices, id: \.self) { dayIndex in
+                    HomeDayCalendarViewStandalone(event: calendarViewModel.dayEntries[dayIndex], homeViewModel: HomeViewModel())
+                }
+            }.tabViewStyle(PageTabViewStyle(indexDisplayMode: .always))
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                
+                ToolbarItem(placement: .principal) {
+                    
+                    HStack {
 
-struct CalendarView_Previews: PreviewProvider {
-    static var previews: some View {
-        CalendarView()
+                        Image(systemName: "chevron.left")
+                            .foregroundColor(.appColorLight)
+                            .onTapGesture {
+                                withAnimation {
+                                    calendarViewModel.index -= 1
+                                }
+                            }
+                        
+//                        Text(calendarViewModel.titleForDay())
+                        Text("\(calendarViewModel.index)")
+                            .frame(width: 100)
+                        
+                        Image(systemName: "chevron.right")
+                            .foregroundColor(.appColorLight)
+                            .onTapGesture {
+                                withAnimation {
+                                    calendarViewModel.index += 1
+                                }
+                            }
+                    }.font(.system(size: 16, weight: .semibold))
+                    
+                }
+                
+            }.onChange(of: calendarViewModel.index) { index in
+//                withAnimation {
+                    if index == 0 {
+                        calendarViewModel.fetchPrev()
+                    }else if index == calendarViewModel.dayEntries.count - 1 {
+                        calendarViewModel.fetchNext()
+                    }
+//                }
+            }
+        }
     }
 }
